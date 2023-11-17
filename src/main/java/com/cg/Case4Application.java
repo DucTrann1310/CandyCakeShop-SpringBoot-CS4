8e31f4a371f54476811c0e6c8f48584dab9e5fe8
DucTrann1310 <prductran1310@gmail.com>
2023-11-17 09:01:33 +0700

    abc

---
+++ src/main/java/com/cg/Case4Application.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Case4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Case4Application.class, args);
+    }
+
+}
---
+++ src/main/resources/static/js/scriptUse.js
@@ -0,0 +1,8 @@
+
+    function redirectToProfile(userId) {
+    window.location.href = `/user?action=profile&id=${user.id}`;
+}
+
+    function changeCursor() {
+    event.target.style.cursor = "pointer";
+}
