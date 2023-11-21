2585f3b03b95e4f8f608ba46374a9095bb13371b
DucTrann1310 <prductran1310@gmail.com>
2023-11-21 17:24:14 +0700

    dependency

--- src/main/java/com/cg/ServletInitializer.java
+++ src/main/java/com/cg/ServletInitializer.java
@@ -1,13 +1,20 @@
-package com.cg;
-
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-public class ServletInitializer extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Case4Application.class);
-    }
-
-}
+//package com.cg;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//import javax.servlet.ServletContext;
+//import javax.servlet.ServletException;
+//
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(Case4Application.class);
+//    }
+//
+//    @Override
+//    public void onStartup(ServletContext servletContext) throws ServletException {
+//
+//    }
+//}
